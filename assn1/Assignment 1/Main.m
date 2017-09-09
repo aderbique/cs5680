39f6a5e6490c18ea504efdc0df37dce68ab765c3
Austin Derbique <austin@unknown4cbb58c2a177.att.net>
2017-09-09 15:06:22 -0600

    sorta working

--- assn1/Assignment 1/Main.m
+++ assn1/Assignment 1/Main.m
@@ -72,7 +72,7 @@ pause;
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Problem 3
+% Problem 4
 C = im2double(B);
 figure;
 imshow(C);
@@ -80,13 +80,120 @@ title('Normalized Grayscale Image');
 
 n=fix(size(C,1)/4);
 disp("The size of n is: " + n);
-%D = cat(1,B(n+1:end,:,:),B(1:n,:,:));
-%imwrite(D,'Austin_D.jpg');
+D = cat(2,cat(2,C(:,1:n,:).^1.25,C(:,n+1:2*n,:)),cat(2,C(:,2*n + 1:3*n,:),C(:,3*n + 1: 4*n,:).^.75));
+imshow(D);
+title("Processed Grayscale Image");
+imwrite(D,'Austin_D.jpg');
 
 disp('-----Finish Solving Problem 4-----');
 disp('Press any key to continue');
 pause;
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem 5
+bw1 = C;
+bw1(bw1<=.3)=0;
+bw1(bw1>.3)=1;
+
+bw2 = C > .3;
+bw3 = imbinarize(C,0.3);
+
+if isequal(bw1,bw3) && isequal(bw2,bw3)
+    disp("Both of my methods worked");
+elseif isequal(bw1,bw3)
+    disp("My method 1 worked but not my method 2")
+elseif isequal(bw2,bw3)
+    disp("My method 2 worked but not my method 1");
+else
+    disp("Both of my two methods did not work");
+end
+
+figure;
+subplot(2,2,1);
+imshow(bw1);
+title('bw1');
+
+subplot(2,2,2);
+imshow(bw2);
+title('bw2');
+
+subplot(2,2,3);
+imshow(bw3);
+title('bw3');
+
+disp('-----Finish Solving Problem 5-----');
+disp('Press any key to continue');
+pause;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem 6
+
+BA = BlurColorIm(A);
+BB = BlurGrayIm(B);
+
+figure;
+subplot(2,2,1);
+imshow(A);
+title('A');
+
+subplot(2,2,2);
+imshow(B);
+title('B');
+
+subplot(2,2,3);
+imshow(BA);
+title('BA');
+
+subplot(2,2,4);
+imshow(BB);
+title('BB');
+
+disp('-----Finish Solving Problem 6-----');
+disp('Press any key to continue');
+pause;
+close all;
+clear;
+
+function [blurredIm] = BlurGrayIm(oriIm)
+ intImage = integralImage(oriIm);
+ avgH = integralKernel([1 1 8 8], 1/64);
+ J = integralFilter(intImage, avgH);
+ blurredIm = uint8(J);
+end
+
+function [blurredIm] = BlurColorIm(oriIm)
+I = oriIm;
+[row,col] = size(oriIm);
+row = row/2;
+col = col/4;
+
+blurredIm = zeros(row,col,3);
+for i = 0:row-1
+    for j = 0:col-1
+        tolR = 0;
+        tolG = 0;
+        tolB = 0;
+        for x = 1:2
+            for y = 1:4
+                
+                %pixelRGB = squeeze(I(uint8(i+x),uint8(j+y),:))
+                disp(I(444,444))
+                red = I(i+x,j+y,1)
+                grn = I(i+x,j+y,2)
+                blu = I(uint8(i+x),uint8(j+y),3)
+                %disp("The red value is: " + I(5,5,1))
+                %tolR = tolR + int8(I(uint16(i+x),uint16(j+y),1));
+                %tolG = tolG + int8(I(uint16(i+x),uint16(j+y),2));
+                %tolB = tolB + int8(I(i + x, j + y,3));
+            end
+        end
+        blurredIm(i,j,1) = tolR / 8;
+        blurredIm(i,j,2) = tolG / 8;
+        blurredIm(i,j,3) = tolB / 8;
+    end
+end
+end
 
 function [p_maxValue, p_minValue, p_meanValue, p_medianValue] = FindInfo(B)
 p_maxValue = 0;
@@ -118,8 +225,3 @@ p_meanValue = intensityTotal / (row*col);
 p_medianValue = 0;
 %return p_maxValue, p_minValue, p_meanValue, p_medianValue;
 end
-
-
-
-
-
