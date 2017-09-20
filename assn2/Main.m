9c47aceec38427bf78e3930407f80a759765f2a4
Austin Derbique <A01967241@aggies.usu.edu>
2017-09-20 14:08:00 -0600

    created empty files

---
+++ assn2/Main.m
@@ -0,0 +1,7 @@
+%CS5680 Assignment 2
+%Austin Derbique
+%A01967241
+%9/20/17
+
+
+%%%%%%%%% Problem 1 %%%%%%%%%
---
+++ assn2/Scaling.m
@@ -0,0 +1,7 @@
+function [ output_args ] = Scaling( input_args )
+%SCALING Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
