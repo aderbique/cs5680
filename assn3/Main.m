efb32a0925bc1ba22c30790a7682d29d485e185f
Austin Derbique <aderbique@aggiemail.usu.edu>
2017-10-06 19:28:18 -0600

    problem 1 working

--- assn3/Main.m
+++ assn3/Main.m
@@ -56,14 +56,41 @@ pause;
 disp("%%%%%%%%% Problem 1.3 %%%%%%%%%");
 
 
+Moon = imread('Moon.jpg');
+mask5 = [0 -1 0; -1 4 -1; 0 -1 0];
+Moon_Laplacian = uint8(conv2(Moon, mask5));
+Moon_Laplacian(end,:) = [];
+Moon_Laplacian(:,end) = [];
+Moon_Laplacian(end,:) = [];
+Moon_Laplacian(:,end) = [];
+
+Scaled_Moon_Laplacian = uint8(imadjust(Moon_Laplacian));
+
+Enhanced_Moon = Moon + Moon_Laplacian;
 
 
 
+figure;
+subplot(2,2,1);
+imshow(Moon);
+title('Orginal Image');
 
+subplot(2,2,2);
+imshow(Moon_Laplacian);
+title('Filtered Image');
 
+subplot(2,2,3);
+imshow(Scaled_Moon_Laplacian);
+title('Scaled Filtered Image');
 
+subplot(2,2,4);
+imshow(Enhanced_Moon);
+title('Enhanced Image');
 
-
+disp('-----Finish Solving Problem 1.3-----');
+disp('Press any key to continue');
+pause;
+disp("%%%%%%%%% Problem 2.1 %%%%%%%%%");
 
 
 
