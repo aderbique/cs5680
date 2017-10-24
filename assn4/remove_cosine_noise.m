ffc3b2dff063dee9aa92bfc58e8c02a0fe0c52e0
Austin Derbique <austin@localhost.localdomain>
2017-10-23 23:09:14 -0600

    slow progress...

--- assn4/remove_cosine_noise.m
+++ assn4/remove_cosine_noise.m
@@ -1,7 +1,19 @@
 function [filteredIm] = remove_cosine_noise(Im)
 %UNTITLED9 Summary of this function goes here
 %   Detailed explanation goes here
+FBoy = fft2(double(Im));
+
+% Problem 4.2
+CFBoy = fftshift(FBoy);
+
+magCFBoy = abs(CFBoy);
+
+histogram(magCFBoy);
 
 
+DFT_transform_Im = magCFBoy;
+
+filteredIm = ifft2(DFT_transform_Im);
+% filteredIm = uint8(((filteredIm + min(min(filteredIm)))/max(max(filteredIm)))*255);
 end
 
