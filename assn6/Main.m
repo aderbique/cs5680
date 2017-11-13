6a63ddcab003536ff3f286ffba34d017ff3c16c5
Austin Derbique <A01967241@aggies.usu.edu>
2017-11-13 15:19:39 -0700

    added main file

---
+++ assn6/Main.m
@@ -0,0 +1,39 @@
+%Austin Derbique
+%A01967241
+%Assignment 6 Main Function
+
+disp("%%%%%%%%% Problem 1 %%%%%%%%%");
+%Problem 1.1
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem 1.2
+
+
+disp('-----Finish Solving Problem 1----');
+disp('Press any key to continue');
+pause; 
+disp("%%%%%%%%% Problem 2 %%%%%%%%%");
+% Problem 2.1
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem 2.2
+
+
+disp('-----Finish Solving Problem 2----');
+disp('Press any key to continue');
+pause; 
+disp("%%%%%%%%% Problem 3 %%%%%%%%%");
+% Problem 3.1
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Problem 3.2
+
+
+disp('-----Finish Solving Problem 3----');
