43b3c6b1365cdb8528143f963d3546fe77788410
Austin Derbique <aderbique@aggiemail.usu.edu>
2017-11-15 11:13:31 -0700

    cleaning code, fixed image

--- assn6/Main.m
+++ assn6/Main.m
@@ -10,7 +10,6 @@ huespace = hsv(:,:,1);
 saturationspace = hsv(:,:,2);
 valuespace = hsv(:,:,3);
 
-
 threshIm = imbinarize(huespace,.1); %converts to binary image with threshold of .1 to isolate ball
 
 isolatedIm = medfilt2(threshIm,[15,15]); %gets rid of salt and pepper
@@ -33,7 +32,6 @@ end
 
 ballWithCentroid = insertMarker(rgb,[centroidx centroidy],'color','blue','size',15);
 
-
 figure;
 subplot(2,2,1);
 imshow(rgb);
@@ -54,7 +52,6 @@ title("Isolated Im");
 figure;
 imshow(ballWithCentroid);
 title("Original Image with Centroid Cross");
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 % Problem 1.2 
@@ -110,26 +107,13 @@ disp("----------- Problem 2 Simple Color Image Retrieval -----------");
 Horse1 = imread('Horse1.jpg');
 histHorse1 = CalNormalizedHSVHist(Horse1,4,4,4);
 
-% figure;
-% subplot(2,2,1);
-% imshow(Horse1);
-% title("Horse1.jpg");
-% 
-% subplot(2,2,2);
-% bar(histHorse1);
-% axis([0,64 0 50000]);
-% xtickformat(1);
-% title("Histogram of Horse1.jpg")
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Problem 2.2
 
 Elephant1 = imread('Elephant1.jpg');
 Elephant2 = imread('Elephant2.jpg');
-
 Horse1 = imread('Horse1.jpg');
 Horse2 = imread('Horse2.jpg');
-
 H1 = CalNormalizedHSVHist(Horse1,4,4,4);
 H2 = CalNormalizedHSVHist(Horse2,4,4,4);
 E1 = CalNormalizedHSVHist(Elephant1,4,4,4);
@@ -140,122 +124,39 @@ subplot(2,2,1);
 bar(H1);
 axis([0,64 0 0.4])
 title("Horse 1");
-
 subplot(2,2,2);
 bar(H2);
 axis([0,64 0 0.4])
 title("Horse 2");
-
 subplot(2,2,3);
 bar(E1);
 axis([0,64 0 0.4])
 title("Elephant 1");
-
 subplot(2,2,4);
 bar(E2);
 axis([0,64 0 0.4])
 title("Elephant 2");
 
-%ImageDB = [Horse1,Horse2,Elephant1,Elephant2];
 ImageDB = {'Horse1','Horse2','Elephant1','Elephant2';Horse1,Horse2,Elephant1,Elephant2;H1,H2,E1,E2;0,0,0,0};
-
-H1_sim = ImageSimilarity(Horse1,ImageDB);
-H2_sim = ImageSimilarity(Horse2,ImageDB);
-E1_sim = ImageSimilarity(Elephant1,ImageDB);
-E2_sim = ImageSimilarity(Elephant2,ImageDB);
-
-
-%%%%%%%%
-%Query 1
-figure;
-subplot(2,2,1);
-imshow(H1_sim{2,1});
-title(H1_sim{1,1} + ", Match: " + H1_sim{4,1} + "%");
-
-subplot(2,2,2);
-imshow(H1_sim{2,2});
-lol = H1_sim{2,2} + ", Match: " + H1_sim{4,2} + "%";
-title(lol);
-
-subplot(2,2,3);
-imshow(H1_sim{2,3});
-title(H1_sim{1,3} + ", Match: " + H1_sim{4,3} + "%");
-
-subplot(2,2,4);
-imshow(H1_sim{2,4});
-title(H1_sim{1,4} + ", Match: " + H1_sim{4,4} + "%");
-
-
-%%%%%%%%
-%Query 2
-
-figure;
-subplot(2,2,1);
-imshow(H2_sim{2,1});
-title(H2_sim{1,1} + ", Match: " + H2_sim{4,1} + "%");
-
-subplot(2,2,2);
-imshow(H2_sim{2,2});
-title(H2_sim{2,2} + ", Match: " + H2_sim{4,2} + "%");
-
-subplot(2,2,3);
-imshow(H2_sim{2,3});
-title(H2_sim{1,3} + ", Match: " + H2_sim{4,3} + "%");
-
-subplot(2,2,4);
-imshow(H2_sim{2,4});
-title(H2_sim{1,4} + ", Match: " + H2_sim{4,4} + "%");
-
-
-%%%%%%%%
-%Query 3
-
-figure;
-subplot(2,2,1);
-imshow(E1_sim{2,1});
-title(E1_sim{1,1} + ", Match: " + E1_sim{4,1} + "%");
-
-subplot(2,2,2);
-imshow(E1_sim{2,2});
-title(E1_sim{2,2} + ", Match: " + E1_sim{4,2} + "%");
-
-subplot(2,2,3);
-imshow(E1_sim{2,3});
-title(E1_sim{1,3} + ", Match: " + E1_sim{4,3} + "%");
-
-subplot(2,2,4);
-imshow(E1_sim{2,4});
-title(E1_sim{1,4} + ", Match: " + E1_sim{4,4} + "%");
-
-
-%%%%%%%%
-%Query 4
-
-figure;
-subplot(2,2,1);
-imshow(E2_sim{2,1});
-title(E2_sim{1,1} + ", Match: " + E2_sim{4,1} + "%");
-
-subplot(2,2,2);
-imshow(E2_sim{2,2});
-title(E2_sim{2,2} + ", Match: " + E2_sim{4,2} + "%");
-
-subplot(2,2,3);
-imshow(E2_sim{2,3});
-title(E2_sim{1,3} + ", Match: " + E2_sim{4,3} + "%");
-
-subplot(2,2,4);
-imshow(E2_sim{2,4});
-title(E2_sim{1,4} + ", Match: " + E2_sim{4,4} + "%");
+[unused, numImages] = size(ImageDB);
+for k=1:numImages
+    ImDB = ImageSimilarity (ImageDB{2,k},ImageDB);
+    figure;
+    for i = 1:numImages
+        subplot(2,2,i);
+        imshow(ImDB{2,i});
+        title(ImDB{1,i} + ", Match: " + ImDB{4,i} + "%");
+    end
+end
 
 disp('-----Finish Solving Problem 2----');
 disp('Press any key to continue');
 pause; 
-disp("----------- Problem 3 A Simple Watermarking Technique in Wavelet Domain -----------");
 %% Problem 3.1
+disp("----------- Problem 3 A Simple Watermarking Technique in Wavelet Domain -----------");
 
 Lena = imread('Lena.jpg');
-
+dwtmode('per');
 [decompLena, S] = wavedec2(Lena,3,'db9');
 [rows,cols] = size(decompLena);
 s = rng(1,'twister');
@@ -376,6 +277,5 @@ bp1_count = bp1_count * 100 ./ (rows*cols);
 bp2_count = bp2_count * 100 ./(rows*cols);
 
 disp("The percent of pixels macthing between b and BP1: " + bp1_count + "% and between b and BP2: " + bp2_count + "%");
-
 
 disp('-----Finish Solving Problem 3----');
